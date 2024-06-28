0617981f5626d4f75356a526ac9e7af1718328bf
BilelIset <bilelbenabdallah24@gmail.com>
2024-06-28 01:42:42 +0100

    Commit apres post notification

--- src/main/java/tn/isetsf/presence/serviceMail/EmailController.java
+++ src/main/java/tn/isetsf/presence/serviceMail/EmailController.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tn.isetsf.presence.Repository.EnstRepo;
+
+import java.time.LocalDate;
+
 @RestController
 @Service
 public class EmailController {
 
     @Autowired
     private EmailService emailService;
-@PostMapping(value = "/sendemail")
+    @Autowired
+    EnstRepo ens;
+
     public Boolean sendEmail( String to,  String subject,  String text) {
 try {
     emailService.sendSimpleEmail(to, subject, text);
@@ -26,5 +32,20 @@ try {
     return false;
 
 }
+    }
+    @PostMapping(value = "/sendemail")
+    public Boolean sendEmail2( @RequestParam String to,  @RequestParam String subject, @RequestParam String text) {
+
+        try {
+            emailService.sendSimpleEmail(to, subject, text);
+            System.out.println("succes d'envoie à : " + to);
+
+            return true;
+        }catch (Exception e){
+            System.out.println("Echec d'envoie à  " + to);
+
+            return false;
+
+        }
     }
 }
