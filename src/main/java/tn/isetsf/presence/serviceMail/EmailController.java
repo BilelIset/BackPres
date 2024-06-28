b651940217c40248ed9fe116a7221f3a352e7030
BilelIset <bilelbenabdallah24@gmail.com>
2024-06-28 01:26:11 +0100

    Commit apres post notification

--- src/main/java/tn/isetsf/presence/serviceMail/EmailController.java
+++ src/main/java/tn/isetsf/presence/serviceMail/EmailController.java
@@ -4,15 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@RestController
 @Service
 public class EmailController {
 
     @Autowired
     private EmailService emailService;
-
+@PostMapping(value = "/sendemail")
     public Boolean sendEmail( String to,  String subject,  String text) {
 try {
     emailService.sendSimpleEmail(to, subject, text);
