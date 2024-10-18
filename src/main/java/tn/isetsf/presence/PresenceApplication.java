5c2a1ac08dc70900e7d5de2d60d6ee1a418cedbe
BilelIset <bilelbenabdallah24@gmail.com>
2024-10-18 18:51:35 +0100

    Commit apres migration spring mvc thymeleay 1

--- src/main/java/tn/isetsf/presence/PresenceApplication.java
+++ src/main/java/tn/isetsf/presence/PresenceApplication.java
@@ -26,7 +26,10 @@ public class PresenceApplication {
 	private AppRoleRepo appRoleRepo;
 	@Autowired
 	private AppUserRepo appUserRepo;
-
+	@Bean
+	PasswordEncoder passwordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
 
 
 	@Bean
@@ -37,47 +40,48 @@ public class PresenceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(PresenceApplication.class, args);
+	}}
 
-		int index = 0, indexdouble = 0;
-
-		LocalTime time = LocalTime.now();
-		if (time.isAfter(LocalTime.of(8, 15)) && time.isBefore(LocalTime.of(9, 45))) {
-			index = 1;
-		} else if (time.isAfter(LocalTime.of(9, 45)) && time.isBefore(LocalTime.of(11, 30))) {
-			index = 2;
-		} else if (time.isAfter(LocalTime.of(11, 30)) && time.isBefore(LocalTime.of(13, 15))) {
-			index = 3;
-		} else if (time.isAfter(LocalTime.of(13, 15)) && time.isBefore(LocalTime.of(14, 50))) {
-			index = 4;
-		} else if (time.isAfter(LocalTime.of(14, 50)) && time.isBefore(LocalTime.of(16, 25))) {
-			index = 5;
-		} else if (time.isAfter(LocalTime.of(16, 25)) && time.isBefore(LocalTime.of(18, 0))) {
-			index = 6;
-
-
-		}
-
-
-		if (time.isAfter(LocalTime.of(8, 15)) && time.isBefore(LocalTime.of(11, 30))) {
-			indexdouble = 7;
-		} else if (time.isAfter(LocalTime.of(9, 45)) && time.isBefore(LocalTime.of(13, 15))) {
-			indexdouble = 8;
-		} else if (time.isAfter(LocalTime.of(11, 30)) && time.isBefore(LocalTime.of(14, 50))) {
-			indexdouble = 11;
-		} else if (time.isAfter(LocalTime.of(13, 15)) && time.isBefore(LocalTime.of(16, 25))) {
-			indexdouble = 9;
-		} else if (time.isAfter(LocalTime.of(14, 50)) && time.isBefore(LocalTime.of(18, 0))) {
-			indexdouble = 10;
-		} else {
-			indexdouble = 0;
-		}
-		System.out.println("simple = " + index + " double = " + indexdouble);
-
-	}
-
-	@Bean
-	CommandLineRunner start(AppUserInterfaceImpl appUserInterface) {
-		return args -> {
+//		int index = 0, indexdouble = 0;
+//
+//		LocalTime time = LocalTime.now();
+//		if (time.isAfter(LocalTime.of(8, 15)) && time.isBefore(LocalTime.of(9, 45))) {
+//			index = 1;
+//		} else if (time.isAfter(LocalTime.of(9, 45)) && time.isBefore(LocalTime.of(11, 30))) {
+//			index = 2;
+//		} else if (time.isAfter(LocalTime.of(11, 30)) && time.isBefore(LocalTime.of(13, 15))) {
+//			index = 3;
+//		} else if (time.isAfter(LocalTime.of(13, 15)) && time.isBefore(LocalTime.of(14, 50))) {
+//			index = 4;
+//		} else if (time.isAfter(LocalTime.of(14, 50)) && time.isBefore(LocalTime.of(16, 25))) {
+//			index = 5;
+//		} else if (time.isAfter(LocalTime.of(16, 25)) && time.isBefore(LocalTime.of(18, 0))) {
+//			index = 6;
+//
+//
+//		}
+//
+//
+//		if (time.isAfter(LocalTime.of(8, 15)) && time.isBefore(LocalTime.of(11, 30))) {
+//			indexdouble = 7;
+//		} else if (time.isAfter(LocalTime.of(9, 45)) && time.isBefore(LocalTime.of(13, 15))) {
+//			indexdouble = 8;
+//		} else if (time.isAfter(LocalTime.of(11, 30)) && time.isBefore(LocalTime.of(14, 50))) {
+//			indexdouble = 11;
+//		} else if (time.isAfter(LocalTime.of(13, 15)) && time.isBefore(LocalTime.of(16, 25))) {
+//			indexdouble = 9;
+//		} else if (time.isAfter(LocalTime.of(14, 50)) && time.isBefore(LocalTime.of(18, 0))) {
+//			indexdouble = 10;
+//		} else {
+//			indexdouble = 0;
+//		}
+//		System.out.println("simple = " + index + " double = " + indexdouble);
+//
+//	}
+//
+//	@Bean
+//	CommandLineRunner start(AppUserInterfaceImpl appUserInterface) {
+//		return args -> {
 
 //appUserInterface.AddUser(new AppUser(null,"user1","1234",new ArrayList<>()));
 //			appUserInterface.AddUser(new AppUser(null,"admin","1234",new ArrayList<>()));
@@ -91,16 +95,13 @@ public class PresenceApplication {
 //			appUserInterface.AddRoleToUser("admin","AGENT");
 //
 ////appUserRepo.deleteAll();
-			System.out.println(appUserInterface.ListUser());
+//			System.out.println(appUserInterface.ListUser());
+//
+//		};
+//	}
 
-		};
-	}
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-}
+
 
 
 
